78688ba325e5993030e0cdbab540db075655ced1
Teman Beck <temanbeck@yahoo.com>
2022-04-15 01:45:50 -0400

    Text: updated About Me bibliography

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,6 +3,7 @@ import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCss3, faGitAlt, faJsSquare, faReact, faNodeJs, faJava } from '@fortawesome/free-brands-svg-icons';
+import Loader from 'react-loaders';
 
 const About = () => {
 
@@ -11,11 +12,12 @@ const About = () => {
   useEffect(() => {
     setTimeout(() => {
       setLetterClass('text-animate-hover')
-    }, 2500)
+    }, 3000)
   }, []);
 
   return (
-    <div className='container about-page'>
+    <>
+      <div className='container about-page'>
       <div className='text-zone'>
         <h1>
           <AnimatedLetters letterClass={letterClass}
@@ -23,11 +25,11 @@ const About = () => {
             idx={15}
           />
         </h1>
-        <p> Software engineer who has experience operating in an agile environment. My tech stack includes but is not limited to Postgres, Express.js, React.js, Node.js, utilizing Sequalize as an ORM. I have experience utilizing JavaScript testing frameworks such as Mocha and Jasmine, as well as implementing new ES6 features and changes. My experience in data store libraries is limited to Redux, but have the amplitude to learn others. Although I operate as a full stack engineer, I thrive in environments which cater to my design skill for building out complex API for client consumption and data manipulation.
+        <p> <strong>Software engineer</strong> who has experience operating in an agile environment. His tech stack includes but is not limited to <strong>Postgres, Express.js, React.js, Node.js</strong>, utilizing  <strong>Sequalize</strong> as ORM. He has experience utilizing <strong>JavaScript testing frameworks</strong> such as <strong>Mocha</strong> and <strong>Jasmine</strong>, as well as implementing new ES6 features. His experience in data store libraries is limited to Redux, but could quickly ramp up to utilize others. Although he operates as a full stack engineer, his natural talent allows him to create unique system designs and back end architectures, allowing for the creation of applications that are scalable and maintainable. He is also a strong advocate of the Agile methodology, and has experience in the Scrum methodology.
         </p>
-        <p> I spent 8 years as an Electrical Engineer for the US Navy. During this time, I gained valuable experience as a Work Center Supervisor, where I managed and mentored 13 junior sailors. The ability to provide value through teaching and disseminating information to both my team and potential clients, enabled continuous deployment of features and services.
+        <p> Spent 8 years as an Electrical Engineer for the US Navy. During this time, he gained valuable experience as a Work Center Supervisor, where he managed and mentored 13 junior sailors. The ability to provide value through teaching and disseminating information to both his team and potential clients, enabled continuous deployment of features and services.
         </p>
-        <p>Lastly, I seek to combine all acquired experience in the fields of electricity, electronics, blockchain, business management, and finance to create products and services that speak directly to consumers and clients
+        <p>Lastly, he seeks to combine all acquired experience in the fields of electricity, electronics, blockchain, business management, and finance to create products and services that speak directly to consumers and clients alike.
         </p>
       </div>
 
@@ -53,7 +55,9 @@ const About = () => {
             </div>
           </div>
         </div>
-    </div>
+      </div>
+      <Loader type="pacman" />
+    </>
   )
 }
 
--- src/components/Home/Logo/index.js
+++ src/components/Home/Logo/index.js
@@ -3,6 +3,7 @@ import gsap from 'gsap-trial';
 import DrawSVGPlugin from "gsap-trial/DrawSVGPlugin";
 import LogoT from '../../../assets/images/logo-t.png';
 import './index.scss';
+import Loader from "react-loaders";
 
 const Logo = () => {
   const bgRef = useRef()
@@ -37,33 +38,37 @@ const Logo = () => {
   }, [])
 
   return (
-    <div className="logo-container" ref={bgRef}>
-      <img
-        className="solid-logo"
-        ref={solidLogoRef}
-        src={LogoT}
-        alt="A huge letter T"
-      />
+    <>
+      <div className="logo-container" ref={bgRef}>
+        <img
+          className="solid-logo"
+          ref={solidLogoRef}
+          src={LogoT}
+          alt="A huge letter T"
+        />
 
-      <svg
-        width="559pt"
-        height="897pt"
-        version="1.0"
-        viewBox="0 0 559 897"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <g
-          className="svg-container"
-          transform="translate(0 457) scale(.1 -.1)"
-          fill="none"
+        <svg
+          width="559pt"
+          height="897pt"
+          version="1.0"
+          viewBox="0 0 559 897"
+          xmlns="http://www.w3.org/2000/svg"
         >
-          <path
-            ref={outlineLogoRef}
+          <g
+            className="svg-container"
+            transform="translate(0 457) scale(.1 -.1)"
+            fill="none"
+          >
+            <path
+              ref={outlineLogoRef}
 
-          />
-        </g>
-      </svg>
-    </div>
+            />
+          </g>
+        </svg>
+      </div>
+      <Loader type="pacman" />
+    </>
+
   )
 }
 

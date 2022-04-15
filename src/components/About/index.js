d165f5a067184fdc366ccd5efaf12a742e786fec
Teman Beck <temanbeck@yahoo.com>
2022-04-15 00:38:41 -0400

    Style: implemented animated skill cube

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCss3, faGitAlt, faHtml5, faJsSquare, faReact, faAngular } from '@fortawesome/free-brands-svg-icons';
 
 const About = () => {
 
@@ -28,6 +30,29 @@ const About = () => {
         <p>Lastly, I seek to combine all acquired experience in the fields of electricity, electronics, blockchain, business management, and finance to create products and services that speak directly to consumers and clients
         </p>
       </div>
+
+      <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faAngular} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
+        </div>
     </div>
   )
 }

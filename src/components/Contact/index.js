da22869b5e15ddde90e6c09c2231c3991a0fc337
Teman Beck <temanbeck@yahoo.com>
2022-10-21 00:14:04 -0400

    Implemented Contact Me form functionality.

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,9 +1,12 @@
-import { useEffect, useState } from 'react'
-import Loader from 'react-loaders'
+import { useEffect, useState, useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import Loader from 'react-loaders';
 import AnimatedLetters from '../AnimatedLetters';
 import './index.scss';
+import styled from "styled-components";
 
 const Contact = () => {
+  const form = useRef();
   const [letterClass, setLetterClass] = useState('text-animate')
 
   useEffect(() => {
@@ -12,8 +15,20 @@ const Contact = () => {
     }, 3000)
   }, [])
 
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_5y88m2y', 'template_01r1ykk', form.current, '4ZxwFGLwtlN5OPymC')
+      .then((result) => {
+        console.log(result.text);
+      }, (error) => {
+        console.log(error.text);
+      })
+      e.target.reset();
+  }
+
   return (
-    <>
+    <StyledContactForm>
       <div className="container contact-page">
         <div className="text-zone">
           <h1>
@@ -23,26 +38,64 @@ const Contact = () => {
                 idx={15}
               />
           </h1>
-          <h2>
-            <a href="mailto:temanbeck@yahoo.com" class="footer-link">
-              temanbeck@yahoo.com
-            </a>
-            <br />
-            <br />
-            Page under construction :
-            <br />
-            <br />
-            Click the LinkedIn icon below to connect.
-
-            <br />
-
-            Thank you!
-          </h2>
+         <form ref={form} onSubmit={sendEmail}>
+          <input type="text" placeholder="Full Name" name="user_name" required />
+          <input type="email" placeholder="Email" name="user_email" required />
+          <input type="text" placeholder="Subject" name="subject" required />
+          <textarea name="message" cols="30" rows="10" />
+          <button type="submit"> Send</button>
+         </form>
         </div>
       </div>
       <Loader type="pacman" />
-    </>
+    </StyledContactForm>
   )
 }
 
 export default Contact
+
+const StyledContactForm = styled.div`
+  width: 400px;
+  form {
+    display: flex;
+    align-items: flex-start;
+    flex-direction: column;
+    width: 100%;
+    font-size: 16px;
+    input {
+      width: 100%;
+      height: 35px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(220, 220, 220);
+      &:focus {
+        border: 2px solid rgba(0, 206, 158, 1);
+      }
+    }
+    textarea {
+      max-width: 100%;
+      min-width: 100%;
+      width: 100%;
+      max-height: 100px;
+      min-height: 100px;
+      padding: 7px;
+      outline: none;
+      border-radius: 5px;
+      border: 1px solid rgb(220, 220, 220);
+      &:focus {
+        border: 2px solid rgba(0, 206, 158, 1);
+      }
+    }
+    label {
+      margin-top: 1rem;
+    }
+    input[type="submit"] {
+      margin-top: 2rem;
+      cursor: pointer;
+      background: rgb(249, 105, 14);
+      color: white;
+      border: none;
+    }
+  }
+`;
